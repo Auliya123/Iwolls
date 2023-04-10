1b46d3e520f522dd5facbb2251d21904d6208e7e
michellead <auliyamichellea@gmail.com>
2023-04-10 15:20:40 +0700

    Initial Commit

---
+++ Iwolls/IwollsApp.swift
@@ -0,0 +1,17 @@
+//
+//  IwollsApp.swift
+//  Iwolls
+//
+//  Created by Auliya Michelle Adhana on 10/04/23.
+//
+
+import SwiftUI
+
+@main
+struct IwollsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
